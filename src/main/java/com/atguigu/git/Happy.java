6a64405fc3a10c84ab21a8bfb57189af8d168148
tom_glb <goodMorning_glb@atguigu.com>
2021-01-06 21:34:46 +0800

    test...

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,7 +3,7 @@ package com.atguigu.git;
 public class Happy {
 
 	public static void main(String[] args) {
-		System.out.println("left ....");
-		System.out.println("right ....");
+		System.out.println("left....");
+		System.out.println("right....");
 	}
 }
